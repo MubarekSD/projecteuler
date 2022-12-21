a1cd5224953abbf4249f8164e4fc2b96eed4e06a
Mubarek Seid <mubareksd@gmail.com>
2022-12-21 13:43:32 +0300

    c

--- c/1.c
+++ c/1.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-    int temp, i = 1, j = 2, sum = 0;
-    while (i <= 4000000) {
-        if (i % 2 == 0)
-            sum += i;
-        temp = i + j;
-        i = j;
-        j = temp;
-    }
-    printf("%d\n", sum);
-    return (0);
+	int sum = 0;
+
+	for (int i = 0; i < 1000; i++)
+		if ((i % 3 == 0) || (i % 5 == 0))
+			sum += i;
+
+	printf("%d\n", sum);
+
+	return (0);
 }
