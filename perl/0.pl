b5fa04bc4714823d7d1c69f7c285773bfc95efa9
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-02 22:56:03 +0300

    update

--- perl/0.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env perl
-$sum = 0;
-for ($i = 0; $i <= 1000; $i++){
-    if (($i % 3 == 0) || ($i % 5 == 0)) {
-        $sum += $i;
-    }
-}
-
-print "$sum\n";
--- ruby/0.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-sum = 0
-for i in 1..1000
-    if (i % 3 == 0) || (i % 5 == 0)
-        sum += i
-    end
-end
-
-puts sum
